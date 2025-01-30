39ecfcbda2ea937da2a8d53b26921149d46f35e5
Aika-vrdj <151849141+Aika-vrdj@users.noreply.github.com>
2025-01-30 12:22:14 -0500

    Update ArtistLogo.tsx

--- src/components/ArtistLogo.tsx
+++ src/components/ArtistLogo.tsx
@@ -4,7 +4,7 @@ export default function ArtistLogo() {
   return (
     <div className="flex justify-center mb-16">
       <img
-        src="https://github.com/Aika-vrdj/takemyhand/blob/main/logo%20site.png"
+        src="https://9tgwg8i0jc.ufs.sh/f/pACH3Nw7YgVWHZN8qeajgIM4UGTsl1pCd8oLzyPt27OivqHK"
         alt="Aika VRDJ"
         className="h-24 object-contain rounded-lg"
       />
